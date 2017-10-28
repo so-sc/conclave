921df4c7d4e4209eff147c75856013cb97c681e9
chaman <chamansullia@gmail.com>
2017-10-28 10:25:03 +0530

    how to get here

--- dump.php
+++ dump.php
@@ -3,15 +3,22 @@ $servername = "localhost";
 $username = "chaman";
 $password = "concl@ve@s@hy@dri";
 $dbname = "sahyadriconclave";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$result = "";
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+if ($mysqli->connect_errno) {
+    echo "Connectin to database failed: ".$mysqli->connect_error;
+   exit();
 }
-$sql = "SELECT * FROM form WHERE 1";
-if ($result=$conn->query($sql) === TRUE) {
-    echo $result;
-  }
-  else{
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
+$sql = "SELECT * FROM form";
+if(!$result = $mysqli->query($sql)){
+    die('There was an error running the query [' . $mysqli->error . ']');
+}
+while($row = $result->fetch_assoc()){
+    //echo $row['gender'] . '<br />';
+    echo $row['userid']." ".$row['fname']." ".$row['lname']." ".$row['gender']." ".$row['org']." ".$row['email']." ".$row['mobile']." ".$row['emergency']." ".$row['address']." ".$row['study']." ".$row['occupation']." ".$row['image']." ".$row['ip']." ".$row['useragent']." ".$row['registered']." ".$row['status']."<br />"."<br />";
+}
+/* free result set */
+$result->free();
+/* close connection */
+$mysqli->close();
 ?>
